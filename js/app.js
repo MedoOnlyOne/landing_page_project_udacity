4521820f547476a33a781dc89fe82b2bc800c9bf
MedoOnlyOne <mohamed.medhat2199@gmail.com>
2021-07-16 23:11:52 +0200

    creat nav bar

--- js/app.js
+++ js/app.js
@@ -1,57 +1,29 @@
-/**
- * 
- * Manipulating the DOM exercise.
- * Exercise programmatically builds navigation,
- * scrolls to anchors from navigation,
- * and highlights section in viewport upon scrolling.
- * 
- * Dependencies: None
- * 
- * JS Version: ES2015/ES6
- * 
- * JS Standard: ESlint
- * 
-*/
+// Helper functions
 
-/**
- * Define Global Variables
- * 
-*/
-
-
-/**
- * End Global Variables
- * Start Helper Functions
- * 
-*/
+// add the sections names to the nav menu
+function creatNav(){
+    /*
+    for each section in the DOM creat a li which inner text is the data-nav attribute for the section,
+    add the menu link class to this li,
+    then append it to the ul*/
+    const ul = document.querySelector("#navbar__list");
+    const sections = document.querySelectorAll("section");
+    for (let section of sections){
+        let li = document.createElement("li");
+        li.innerText = section.getAttribute("data-nav");
+        li.classList.add("menu__link");
+        ul.appendChild(li);
+    }
+}
 
 
 
-/**
- * End Helper Functions
- * Begin Main Functions
- * 
-*/
-
-// build the nav
 
 
-// Add class 'active' to section when near top of viewport
-
-
-// Scroll to anchor ID using scrollTO event
-
-
-/**
- * End Main Functions
- * Begin Events
- * 
-*/
-
-// Build menu 
-
-// Scroll to section on link click
-
-// Set sections as active
-
-
+// Apply the logic when the page loads
+document.addEventListener("DOMContentLoaded", ()=>{
+    /* 
+    creat the nav bar 
+    */
+    creatNav();
+});
