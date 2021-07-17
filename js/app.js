1018e207fa2947575d5112b83061b15278f23b6d
MedoOnlyOne <mohamed.medhat2199@gmail.com>
2021-07-17 14:34:07 +0200

    smooth scrolling

--- js/app.js
+++ js/app.js
@@ -3,15 +3,20 @@
 // add the sections names to the nav menu
 function creatNav(){
     /*
-    for each section in the DOM creat a li which inner text is the data-nav attribute for the section,
+    for each section in the DOM creat li which contains anchor tag that has
+    inner text with the value of the data-nav attribute for the section and href with the section id,
     add the menu link class to this li,
+    append this anchor tag to the li,
     then append it to the ul*/
     const ul = document.querySelector("#navbar__list");
     const sections = document.querySelectorAll("section");
     for (let section of sections){
         let li = document.createElement("li");
-        li.innerText = section.getAttribute("data-nav");
-        li.classList.add("menu__link");
+        let a = document.createElement("a");
+        a.innerText = section.getAttribute("data-nav");
+        a.setAttribute("href", `#${section.getAttribute("id")}`);
+        a.classList.add("menu__link");
+        li.appendChild(a);
         ul.appendChild(li);
     }
 }
